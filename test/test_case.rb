6f4394e9d5785403390c8f8b0901a10e47e5c378
Paul R Alexander <palexander@stanford.edu>
2013-04-26 19:23:18 -0700

    Warn about running tests against potential production machines

--- test/test_case.rb
+++ test/test_case.rb
@@ -4,6 +4,25 @@ require_relative "../config/config.rb"
 
 require "test/unit"
 
+# Check to make sure you want to run if not pointed at localhost
+safe_host = Regexp.new(/localhost|ncbo-dev*/)
+unless LinkedData.settings.goo_host.match(safe_host) && LinkedData.settings.search_server_url.match(safe_host) && LinkedData.settings.redis_host.match(safe_host)
+  print "\n\n================================== WARNING ==================================\n"
+  print "** TESTS CAN BE DESTRUCTIVE -- YOU ARE POINTING TO A POTENTIAL PRODUCTION/STAGE SERVER **\n"
+  print "Servers:\n"
+  print "triplestore -- #{LinkedData.settings.goo_host}\n"
+  print "search -- #{LinkedData.settings.search_server_url}\n"
+  print "redis -- #{LinkedData.settings.redis_host}\n"
+  print "Type 'y' to continue: "
+  $stdout.flush
+  confirm = $stdin.gets
+  if !(confirm.strip == 'y')
+    abort("Canceling tests...\n\n")
+  end
+  print "Running tests..."
+  $stdout.flush
+end
+
 ##
 # Base test class. Put shared test methods or setup here.
 class TestCase < Test::Unit::TestCase
