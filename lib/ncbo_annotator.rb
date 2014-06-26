488dd8a000ba3cbafea4b32736c95bb72d05ce83
Michael Dorf <mdorf@stanford.edu>
2014-06-25 18:10:34 -0700

    fixed NCBO-832 - SCTSPA Annotator Cache building error (UTF-8)

--- lib/ncbo_annotator.rb
+++ lib/ncbo_annotator.rb
@@ -543,6 +543,9 @@ module Annotator
       end
 
       def create_term_entry(redis, instance_prefix, ontResourceId, resourceId, label_type, val, semanticTypes)
+        # NCBO-832 - SCTSPA Annotator Cache building error (UTF-8)
+        val = val.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+
         # exclude single-character or empty/null values
         if (val.to_s.strip.length > 2)
           # NCBO-696 - Remove case-sensitive variations on terms in annotator dictionary
